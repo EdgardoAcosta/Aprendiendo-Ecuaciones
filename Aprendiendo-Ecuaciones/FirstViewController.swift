947447ecbc6a7cbb4d682355e4cd37236589cf71
Edgardo Acosta Leal <edgardo_acostaleal@hotmail.com>
2016-10-19 19:34:35 -0500

    Primera carga de archivos

---
+++ Aprendiendo-Ecuaciones/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Aprendiendo-Ecuaciones
+//
+//  Created by Edgardo Acosta on 19/10/16.
+//  Copyright © 2016 Edgardo Acosta. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
