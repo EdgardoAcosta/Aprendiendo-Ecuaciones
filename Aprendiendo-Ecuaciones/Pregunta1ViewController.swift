2f318571e50f4c8d043ff04abae07b02de049ad3
EdgardoAcosta <edgardo_acostaleal@hotmail.com>
2016-11-02 18:23:22 -0600

    Actualizacion de preguntas random

--- Aprendiendo-Ecuaciones/Pregunta1ViewController.swift
+++ Aprendiendo-Ecuaciones/Pregunta1ViewController.swift
@@ -20,9 +20,14 @@ class Pregunta1ViewController: UIViewController {
     
     @IBOutlet weak var imageRespuesta4: UIImageView!
     
+    @IBOutlet weak var btRespuesta2: UIButton!
+    
+    
+    
     var arregloRespuestas : [UIImage] = []
     var arregloImagenes : [UIImageView] = []
     var respuesta :  Int!
+    var imagenRespuesta : UIImageView!
     
     @IBOutlet weak var tfPregunta: UILabel!
     
@@ -59,6 +64,10 @@ class Pregunta1ViewController: UIViewController {
         tfPregunta.text = Pregunta
         respuesta = verificarRespuesta()
         asignarImages()
+
+        
+        
+        
         
         }
 
@@ -114,19 +123,48 @@ class Pregunta1ViewController: UIViewController {
         var aux = 0
         var random : Int
         
-    
-        while a < 3 {
-
+        //Elige imagenes aleatoriamente
+        while a < 4 {
+            //Elejimos un numero de imagen
             random = Int (arc4random_uniform(UInt32(5 - a)) )
+            
+            //Obtenemos valor
             aux = arreglo[random]
+            
+            //Set a la imagen en la posicion a (0 - 3)
             arregloImagenes[a].image = arregloRespuestas[aux]
+            
+            //Swap del valor de la ultima posicion a la posicion obtenida por random
             arreglo[random] = arreglo [5 - a]
+            
+            //Asignar valor de random a la ultima posicion
             arreglo[5 - a] = aux
             
+            //Aumenta valor
             a = a + 1
-            
         }
-        arregloImagenes[3].image = arregloRespuestas[respuesta]
+        
+        //Varibale para detectar si la respuesta ya fue escogida
+        var flag : Bool = false
+        
+        for i in 0...3 {
+            if arregloImagenes[i].image == arregloRespuestas[respuesta] {
+                imagenRespuesta = arregloImagenes[i]
+                flag = true
+                break
+            }
+        }
+        
+        if !flag {
+            //Asignamos a ultima posicion la imagen correcta
+            arregloImagenes[3].image = arregloRespuestas[respuesta]
+            
+            //Guradamos imagen correcta para comparar
+            imagenRespuesta = arregloImagenes[3]
+        }
+
+        print(respuesta)
+        
         
         a = 0
         var auxImagePosition : UIImageView
@@ -142,21 +180,55 @@ class Pregunta1ViewController: UIViewController {
             arreglo[3 - a] = aux
             
             a = a + 1
-            
         }
-        
-        
     }
     
+    @IBAction func tapImagen1(_ sender: AnyObject) {
+        
+        if imagenRespuesta.image == imageRespuesta1.image{
+            print("la 1 es la correcta")
+        }
+        else{
+            print("esta no es 1")
+        }
+    }
+    
+    
+    @IBAction func tapImagen2(_ sender: AnyObject) {
+
+        if imagenRespuesta.image == imageRespuesta2.image{
+            print("la 2 es la correcta")
+        }
+        else{
+            print("esta no es 2")
+        }
+    }
+    
+    @IBAction func tapImagen3(_ sender: AnyObject) {
+
+        if imagenRespuesta.image == imageRespuesta3.image{
+            print("la 3 es la correcta")
+        }
+        else{
+            print("esta no es 3")
+        }
+    }
+    
+    @IBAction func tapImagen4(_ sender: AnyObject) {
+        if imagenRespuesta.image == imageRespuesta4.image{
+            print("la 4 es la correcta")
+        }
+        else{
+            print("esta no es 4")
+        }
+    }
+    
+
         
         
     
 
-    
-    
-    
-    
-    
+
     
     /*
     // MARK: - Navigation
